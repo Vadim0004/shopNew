b615ad47596eea0493698695ace57e866573f1a0
Vadim0004 <vadim.vikentev@mail.ru>
2018-10-30 18:56:40 +0200

    Add CatalogController

---
+++ frontend/controllers/shop/CatalogController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace frontend\controllers\shop;
+
+use yii\web\Controller;
+
+class CatalogController extends Controller
+{
+    public $layout = 'catalog';
+
+    public function actionIndex()
+    {
+
+        return $this->render('index');
+    }
+}
