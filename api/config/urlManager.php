f8bb5c340857ecc6ffe10f762fe4413bdbfed993
Vadim0004 <vadim.vikentev@mail.ru>
2018-11-20 13:22:13 +0200

    add API

---
+++ api/config/urlManager.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @var array $params */
+
+return [
+    'class' => 'yii\web\UrlManager',
+    'enablePrettyUrl' => true,
+    'showScriptName' => false,
+    'rules' => [
+        '' => 'auth/login',
+        '<_a:login|logout>' => 'auth/<_a>',
+
+        '<_c:[\w\-]+>' => '<_c>/index',
+        '<_c:[\w\-]+>/<id:\d+>' => '<_c>/view',
+        '<_c:[\w\-]+>/<_a:[\w-]+>' => '<_c>/<_a>',
+        '<_c:[\w\-]+>/<id:\d+>/<_a:[\w\-]+>' => '<_c>/<_a>',
+    ],
+];
--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -2,6 +2,7 @@
 Yii::setAlias('@common', dirname(__DIR__));
 Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
+Yii::setAlias('@api', dirname(dirname(__DIR__)) . '/api');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
 Yii::setAlias('@shop', dirname(dirname(__DIR__)) . '/shop');
 Yii::setAlias('@fileCsv', dirname(dirname(__DIR__)) . '/backend/web/uploads/files');
